1e68fb760e07dc6c43b8c1827a39bcb061812832
olga20011 <olgaestherakello@gmail.com>
2021-03-07 13:00:56 +0300

    assignment1

---
+++ olga.js
@@ -0,0 +1,8 @@
+function  fruits(){
+    let fruitList=['strawberries', 'kiwi', 
+    'mangoes', 'tomatoes', 'guava'];
+    fruitList.forEach(element=>console.log(element));
+    console.log(fruitList[0]);
+
+}
+fruits();
